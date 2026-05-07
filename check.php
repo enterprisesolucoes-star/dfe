429ac62bc9bcb15cced08f2f4260b7734c597119
Enterprise Solucoes <contato@esolucoesia.com>
2026-05-06 21:02:02 -0300

    chore: remover arquivos de desenvolvimento expostos no webroot

--- check.php
+++
@@ -1,4 +0,0 @@
-<?php
-try {
-    \ = new PDO('sqlite:database.sqlite'); // Dummy connection just to syntax check
-} catch(Exception \) {}
--- f2.cjs
+++
@@ -1,6 +0,0 @@
-﻿const fs=require('fs');
-let c=fs.readFileSync('src/components/NfceDashboard.tsx','utf8');
-c = c.replace(/NÃƒmero/g, 'Número');
-c = c.replace(/NÃƒmero/g, 'Número'); // Double check variations
-c = c.replace(/Ã°Å¸â€ž/g, '📄');
-fs.writeFileSync('src/components/NfceDashboard.tsx', c, 'utf8');
